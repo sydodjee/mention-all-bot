4f8b22ebaf3ce302ef1f823df20f01f18299baac
sydodjee <125597093+sydodjee@users.noreply.github.com>
2024-12-02 09:35:01 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,95 +1,95 @@
-import os
-import psycopg2
-
-DB_CONNSTRING = os.environ['DB_CONNSTRING']
-
+import sqlite3
 
 class BotDatabase:
-    def __init__(self, filename):
-        self.conn = psycopg2.connect(DB_CONNSTRING)
-
+    def __init__(self, db_file):
+        self.conn = sqlite3.connect(db_file)
         self._add_users_table()
         self._add_chats_table()
 
     def add_user(self, user_id, username):
-        with self.conn.cursor() as cursor:
-            query = '''INSERT INTO users (user_id, username) VALUES (%s, %s) ON CONFLICT (user_id) do UPDATE SET username = %s'''
-            cursor.execute(query, (user_id, username, username))
-            self.conn.commit()
+        with self.conn:
+            self.conn.execute(
+                '''
+                INSERT INTO users (user_id, username) VALUES (?, ?)
+                ON CONFLICT(user_id) DO UPDATE SET username = ?;
+                ''',
+                (user_id, username, username)
+            )
 
     def get_all_users(self):
-        with self.conn.cursor() as cursor:
-            query = '''SELECT user_id, username FROM users'''
-            cursor.execute(query)
-            return cursor.fetchall()
+        with self.conn:
+            return self.conn.execute('SELECT user_id, username FROM users').fetchall()
 
-    def get_users_from_chat(self, group_id):
-        with self.conn.cursor() as cursor:
-            query = '''SELECT c.user_id, u.username 
-                        FROM chats c 
-                        JOIN users u on c.user_id = u.user_id 
-                        WHERE c.chat_id=%s'''
-            cursor.execute(query, (group_id,))
-            return cursor.fetchall()
+    def get_users_from_chat(self, chat_id):
+        with self.conn:
+            return self.conn.execute(
+                '''
+                SELECT c.user_id, u.username
+                FROM chats c
+                JOIN users u ON c.user_id = u.user_id
+                WHERE c.chat_id = ?;
+                ''',
+                (chat_id,)
+            ).fetchall()
 
     def add_user_to_chat(self, chat_id, user_id):
-        with self.conn.cursor() as cursor:
-            query = '''INSERT INTO chats (chat_id, user_id) VALUES (%s, %s) ON CONFLICT DO NOTHING'''
-            cursor.execute(query, (chat_id, user_id))
-            self.conn.commit()
+        with self.conn:
+            self.conn.execute(
+                '''
+                INSERT INTO chats (chat_id, user_id) VALUES (?, ?)
+                ON CONFLICT DO NOTHING;
+                ''',
+                (chat_id, user_id)
+            )
 
     def delete_user_from_chat(self, chat_id, user_id):
-        with self.conn.cursor() as cursor:
-            query = '''DELETE from chats WHERE chat_id = %s AND user_id = %s'''
-            cursor.execute(query, (chat_id, user_id))
-            self.conn.commit()
-
-    def update_user_username(self, user_id, new_username):
-        with self.conn.cursor() as cursor:
-            sql_update_query = '''UPDATE users SET username = %s WHERE user_id = %s'''
-            cursor.execute(sql_update_query, (new_username, user_id))
-            self.conn.commit()
+        with self.conn:
+            self.conn.execute(
+                'DELETE FROM chats WHERE chat_id = ? AND user_id = ?',
+                (chat_id, user_id)
+            )
 
     def count_users(self):
-        with self.conn.cursor() as cursor:
-            query = '''SELECT COUNT(user_id) FROM users'''
-            cursor.execute(query)
-            return cursor.fetchone()
+        with self.conn:
+            return self.conn.execute('SELECT COUNT(user_id) FROM users').fetchone()
 
     def count_chats(self):
-        with self.conn.cursor() as cursor:
-            query = '''SELECT COUNT(DISTINCT chat_id) FROM chats'''
-            cursor.execute(query)
-            return cursor.fetchone()
+        with self.conn:
+            return self.conn.execute('SELECT COUNT(DISTINCT chat_id) FROM chats').fetchone()
 
     def count_groups(self):
-        with self.conn.cursor() as cursor:
-            query = '''SELECT COUNT(DISTINCT chat_id) FROM chats WHERE chat_id <> user_id'''
-            cursor.execute(query)
-            return cursor.fetchone()
+        with self.conn:
+            return self.conn.execute(
+                '''
+                SELECT COUNT(DISTINCT chat_id)
+                FROM chats
+                WHERE chat_id <> user_id;
+                '''
+            ).fetchone()
 
     def _add_users_table(self):
-        with self.conn.cursor() as cursor:
-            query = '''CREATE TABLE IF NOT EXISTS 
-                                        users (user_id BIGINT, username VARCHAR(64), PRIMARY KEY (user_id))'''
-            cursor.execute(query)
-            self.conn.commit()
+        with self.conn:
+            self.conn.execute(
+                '''
+                CREATE TABLE IF NOT EXISTS users (
+                    user_id INTEGER PRIMARY KEY,
+                    username TEXT
+                );
+                '''
+            )
 
     def _add_chats_table(self):
-        with self.conn.cursor() as cursor:
-            query = '''
+        with self.conn:
+            self.conn.execute(
+                '''
                 CREATE TABLE IF NOT EXISTS chats (
-                    chat_id BIGINT, 
-                    user_id BIGINT, 
+                    chat_id INTEGER,
+                    user_id INTEGER,
                     PRIMARY KEY (chat_id, user_id),
-                    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE )'''
-            cursor.execute(query)
-            self.conn.commit()
+                    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+                );
+                '''
+            )
 
     def close(self):
         self.conn.close()
-
-
-if __name__ == '__main__':
-    db = BotDatabase('bot_users.db')
-    db.close()
