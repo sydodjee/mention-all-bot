7596b92a32be90d5e9efb528b06b606f8656056f
sydodjee <125597093+sydodjee@users.noreply.github.com>
2024-12-02 09:51:24 +0400

    Update app.py

--- app.py
+++ app.py
@@ -1,66 +1,72 @@
 import os
 import logging
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram.utils.helpers import mention_markdown
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
 from database import BotDatabase
 
+# Настраиваем логирование
 logging.basicConfig(
-    filename='logs.log',
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
-# SQLite-файл для базы данных
-db = BotDatabase('database.db')
+# Получаем токен бота из переменной окружения
+TOKEN = os.getenv("BOT_TOKEN")
+db = BotDatabase()
 
-TOKEN = os.getenv('TOKEN')
 
-def start_command(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Добро пожаловать! Введите /in, чтобы присоединиться.")
+def start(update: Update, context: CallbackContext):
+    """Команда /start"""
+    update.message.reply_text("Привет! Используйте /in, чтобы добавиться в список.")
 
-def in_command(update, context):
-    chat_id = update.effective_chat.id
+
+def in_command(update: Update, context: CallbackContext):
+    """Добавить пользователя в список"""
     user = update.effective_user
-    user_name = user.username or user.first_name or "anonymous"
-    db.add_user(user.id, user_name)
+    chat_id = update.effective_chat.id
+
+    db.add_user(user.id, user.username or user.first_name)
     db.add_user_to_chat(chat_id, user.id)
-    context.bot.send_message(chat_id=chat_id, text=f"Вы добавлены, {user_name}!")
 
-def out_command(update, context):
-    chat_id = update.effective_chat.id
+    update.message.reply_text(f"{user.first_name}, вы добавлены в список!")
+
+
+def out_command(update: Update, context: CallbackContext):
+    """Удалить пользователя из списка"""
     user = update.effective_user
-    user_name = user.username or user.first_name or "anonymous"
-    db.delete_user_from_chat(chat_id, user.id)
-    context.bot.send_message(chat_id=chat_id, text=f"Вы удалены, {user_name}.")
-
-def all_command(update, context):
     chat_id = update.effective_chat.id
-    user_list = db.get_users_from_chat(chat_id)
-    if not user_list:
-        context.bot.send_message(chat_id=chat_id, text="Пользователей нет. Введите /in для добавления.")
+
+    db.delete_user_from_chat(chat_id, user.id)
+
+    update.message.reply_text(f"{user.first_name}, вы удалены из списка!")
+
+
+def all_command(update: Update, context: CallbackContext):
+    """Упомянуть всех участников чата"""
+    chat_id = update.effective_chat.id
+    users = db.get_users_from_chat(chat_id)
+
+    if not users:
+        update.message.reply_text("Список пуст. Добавьтесь с помощью команды /in.")
     else:
-        mentions = [mention_markdown(user_id, username, version=2) for user_id, username in user_list]
-        context.bot.send_message(chat_id=chat_id, text=" ".join(mentions), parse_mode="MarkdownV2")
+        mentions = [f"@{username}" for _, username in users if username]
+        update.message.reply_text(" ".join(mentions))
 
-def stats_command(update, context):
-    users_count = db.count_users()[0]
-    chats_count = db.count_chats()[0]
-    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Всего пользователей: {users_count}\nЧатов: {chats_count}")
 
-def unknown_command(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Неизвестная команда.")
+def main():
+    """Главная функция для запуска бота"""
+    updater = Updater(token=TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
 
-updater = Updater(token=TOKEN, use_context=True)
-dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("in", in_command))
+    dispatcher.add_handler(CommandHandler("out", out_command))
+    dispatcher.add_handler(CommandHandler("all", all_command))
 
-dispatcher.add_handler(CommandHandler('start', start_command))
-dispatcher.add_handler(CommandHandler('in', in_command))
-dispatcher.add_handler(CommandHandler('out', out_command))
-dispatcher.add_handler(CommandHandler('all', all_command))
-dispatcher.add_handler(CommandHandler('stats', stats_command))
-dispatcher.add_handler(MessageHandler(Filters.command, unknown_command))
+    updater.start_polling()
+    updater.idle()
 
-updater.start_polling()
-updater.idle()
+    db.close()
 
-db.close()
+
+if __name__ == "__main__":
+    main()
